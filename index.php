d22b26b6d031f69a58ebd3bfe9cef1e4908c7f04
Dab <luidanielugartechavez@gmail.com>
2022-12-12 10:25:53 -0600

    Se cambio el index anterior por checkout

--- index.php
+++ index.php
@@ -4,42 +4,16 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>E Shop</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous"/>
 
-    <script src="https://www.paypal.com/sdk/js?client-id=AR6KiHAiLXm1JEzbMtaIl6PJHOgFCTw9HIWWbSP-gXOGa-6zvfYTPRYOLTAS3Oy1mXK1nkRpPx1BdjuR&currency=MXN"></script>
 </head>
-
-
 <body>
     
-<div id="paypal-button-container"></div>
 
-<script>
-    paypal.Buttons({
-        style:{
-            color: 'blue',
-            shape: 'pill',
-            label: 'pay'
-        },
-        createOrder: function(data, actions){
-            return actions.order.create({
-                purchase_units: [{
-                    amount : {
-                        value: 100
-                    }
-                }]
-            });
-        },
-        onApprove: function(data, actions){
-            actions.order.capture().then(function (detalles){
-                window.location.href="completado.html"
-            });
-        },
-        onCancel : function(data){
-            alert("Pago cancelado");
-            console.log(data);
-        }
-    }).render('#paypal-button-container');
-</script>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 </html>
